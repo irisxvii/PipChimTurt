32997684f76b1063158b940d0eabf81e7dda0100
iris <iriskurien@gmail.com>
2024-11-23 15:12:55 +0530

    (feat) added more bouncy bois

--- sketch.js
+++ sketch.js
@@ -1,22 +1,27 @@
-let item;
+let items= [];
+let itemCount= 50;
 
 function setup() {
   createCanvas(400, 400);
 
-  item={ 
-    x: random(width),
-    y: random(height),
-    size: 50,
-    veloX: random(1,3),
-    veloY: random(1,3)
+  for(let i=0; i<itemCount; i++){
+    items.push({ 
+      x: random(width),
+      y: random(height),
+      size: 50,
+      veloX: random(1,3),
+      veloY: random(1,3)
+    })
   }
 }
 
 function draw() {
   background(220);
 
-  item.x+= item.veloX;
-  item.y+= item.veloY;
+  for(let i=0; i<items.length; i++){
+    let item= items[i];
+    item.x+= item.veloX;
+    item.y+= item.veloY;
 
   if(item.x < 0 || item.x > width)
     item.veloX*= -1;
@@ -26,4 +31,4 @@ function draw() {
   fill(100, 150, 255);
   noStroke();
   ellipse(item.x, item.y, item.size);
-}
+}}
